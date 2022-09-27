66c77ce1a1d4406a20a8f518827b76be06018da3
necipfazilgocer <114177524+necipfazilgocer@users.noreply.github.com>
2022-09-28 02:16:06 +0300

    Add files via upload

---
+++ palindrom.java
@@ -0,0 +1,24 @@
+package palindrom;
+
+public class palindrom {
+	
+	static boolean ispalindrom(int number) {
+		int temp=number,reverseNumber=0,lastnumber;
+		while(temp!=0) {
+			
+			lastnumber=temp%10;
+			reverseNumber=( reverseNumber*10)+lastnumber;
+			temp/=10;
+		}
+		if(number==reverseNumber)
+		return true;
+		else 
+			return false;
+		
+	}
+	
+	public static void main(String[] args) {
+	System.out.println(ispalindrom(7887));
+	}
+
+}
